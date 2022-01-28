651ae8d7e735e365c799460f90cb22e9bb4dada8
Michele Pugliese <michele.mona987@gmail.com>
2022-01-28 18:17:40 +0100

    Navbar quasi completa, piccoli ritocchi

---
+++ javascripts/index.js
@@ -0,0 +1,6 @@
+let toggle = document.getElementById("toggle");
+let sidebar = document.getElementById("sidebar");
+
+toggle.addEventListener("click",function(){
+    sidebar.classList.toggle("sidebarVisible")
+});
